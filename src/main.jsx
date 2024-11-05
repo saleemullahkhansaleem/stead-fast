9c1d33f60a8a5f28ba6e0c36bb9b3531e50b9c0b
Mubashar Iftikhar <62559139+mubashariftikhar@users.noreply.github.com>
2024-11-05 11:17:35 +0500

    company profile

--- src/main.jsx
+++ src/main.jsx
@@ -24,6 +24,7 @@ import {
   WhoPage,
   WhyPage,
 } from "./pages";
+import CompanyProfile from "./pages/CompanyProfile.jsx";
 
 const router = createBrowserRouter([
   {
@@ -49,6 +50,10 @@ const router = createBrowserRouter([
       {
         path: "what-we-do",
         element: <WhatPage />,
+      },
+      {
+        path: "company-profile",
+        element: <CompanyProfile />,
       },
       {
         path: "contact-us",
