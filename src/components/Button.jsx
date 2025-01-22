c9fef24a520383bb67070488b364c2af829c3452
Saleemullah Khan Saleem <salikhanswati@gmail.com>
2025-01-22 15:43:05 +0500

    fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ export default function Button({
   disabled = false,
   ...props
 }) {
-  const commonClasses = `px-4 py-3 md:px-6 md:py-4 bg-primary text-sm md:text-base ${
+  const commonClasses = `px-5 py-3 bg-primary text-sm ${
     disabled === false ? "hover:bg-secondary" : "cursor-not-allowed"
   } rounded-full text-background transition-colors duration-200 leading-none truncate ${className}`;
 
